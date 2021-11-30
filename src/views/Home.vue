81985dd843c631dc8f6bb3e0ba43bc0aaa36fd9c
Mauro <mauripromotion@gmail.com>
2021-11-30 15:56:07 -0600

    Se Agregan Sass y Bootstrap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-success">Success</button>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
