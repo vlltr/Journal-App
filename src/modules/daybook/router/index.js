bd537fd3f57dcd7b88ece964b6911637826cfce6
Mauro <mauripromotion@gmail.com>
2021-12-01 08:55:34 -0600

    Se configuran estilos y rutas

---
+++ src/modules/daybook/router/index.js
@@ -0,0 +1,7 @@
+export default {
+    name: 'daybook',
+    component: () => import(/* webpackChunkName: "daybook" */ '@/modules/daybook/layouts/DayBookLayout.vue'),
+    children: [
+        
+    ]
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-secondary">Secondary</button>
-    <button class="btn btn-success">Success</button>
+    <button @click="goToDaybook" class="btn btn-primary">Primary</button>
+    <button @click="goToDaybook" class="btn btn-secondary">Secondary</button>
+    <button @click="goToDaybook" class="btn btn-success">Success</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToDaybook() {
+      this.$router.push({ name: 'daybook' })
+    }
+  }
+}
+</script>
