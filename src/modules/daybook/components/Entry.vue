84f0048be3ef2f291e19df74c89dca4290079177
Mauro <mauripromotion@gmail.com>
2021-12-01 11:46:06 -0600

    Se Modifican componentes de entrada de lista

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2">
+      <!--Title-->
+      <div class="entry-title d-flex">
+        <span class="text-success fs-5 fw-bold">1</span>
+        <span class="mx-1 fs-5">Dic</span>
+        <span class="mx-2 fs-5 fw-light">2021</span>
+      </div>
+
+      <div class="entry-description">
+          Officia laborum laboris Lorem nostrud occaecat do eiusmod velit quis in quis cillum qui commodo. Consequat pariatur excepteur sint et anim cillum eu ad officia elit laboris tempor.
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover{
+        background: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,13 +1,40 @@
 <template>
-  <h1>Entry List</h1>
+  <div class="entry-list-container">
+      <div class="px-2 pt-2">
+          <input 
+            type="text"
+            class="form-control"
+            placeholder="Search"
+          >
+      </div>
+
+      <div class="entry-scroll-area">
+          <h2
+            v-for="item in 10"
+            :key="item"
+          >
+          <Entry />
+          </h2>
+      </div>
+  </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue'
 export default {
-
+    components: {
+        Entry: defineAsyncComponent(() => import('./Entry.vue') ),
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.entry-list-container{
+    border-right: 1px solid #2c3e50;
+    height: calc( 100vh - 56px );
+}
+.entry-scroll-area{
+    height: calc( 100vh - 110px );
+    overflow: scroll;
+}
 </style>
