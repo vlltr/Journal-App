0cb917deb5ef2b6c430b1c2b832ed98361b392b7
Mauro <mauripromotion@gmail.com>
2021-12-01 14:06:26 -0600

    Se agrega boton flotante

--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -2,6 +2,10 @@ export default {
     name: 'daybook',
     component: () => import(/* webpackChunkName: "daybook" */ '@/modules/daybook/layouts/DayBookLayout.vue'),
     children: [
-        
+        {
+            path: '',
+            name: 'no-entry',
+            component: () => import(/* webpackChunkName: "noEntry" */ '@/modules/daybook/views/NoEntrySelected.vue')
+        }
     ]
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
 export default {
   methods: {
     goToDaybook() {
-      this.$router.push({ name: 'daybook' })
+      this.$router.push({ name: 'no-entry' })
     }
   }
 }
